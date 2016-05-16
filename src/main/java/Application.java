d715fb835089b4f0be2c6bf17afc81474a675040
funnyling <funnyling444@gmail.com>
2016-05-16 18:34:18 +0300

    .gitignore + classes

---
+++ src/main/java/Application.java
@@ -0,0 +1,7 @@
+/**
+ * @author ntishkevich
+ */
+public class Application {
+
+
+}
---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * @author ntishkevich
+ */
+public class ApplicationTest {
+
+    @Test
+    public void test() {
+
+    }
+}
